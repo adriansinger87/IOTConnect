3a2dde09c37b3c0c291e320a1d1a5270cab9ee00
Andreas Philipp <pille21@hotmail.com>
2019-03-27 17:36:12 +0100

    Dynamic data for chart

--- IOTConnect.Solution/IOTConnect.WebAPI/wwwroot/js/_home.js
+++ IOTConnect.Solution/IOTConnect.WebAPI/wwwroot/js/_home.js
@@ -1,16 +1,15 @@
 ﻿$(function () {
 
     var appVue = getAppVue();
-    var appVue2 = getAppVue2();
     initActions();
- 
+
     console.info("_home.js done");
     // -- functions
 
     function initActions() {
         $(document).ready(function () {
             appVue.getDevices();
-            appVue2.getSenor0815();
+
         });
     }
 
@@ -29,71 +28,6 @@
         });
     }
 
-    function getAppVue2() {
-        return new Vue({
-            el: '#app-vue2',
-            data: {
-                jsonData: null
-            },
-            methods: {
-                getSenor0815: getSenor0815
-            },
-            mounted() {
-                getAppChart(this.jsonData);
-            },
-            updated: function () {
-                getAppChart(this.jsonData);
-            }
-        });
-    }
-
-    function getAppChart(jsonData) {
-        
-        var values = [];
-        var timeArray = [];
-        var valuesArray = [];
-        //TODO @ ap  connect to real data
-        if (jsonData == null) {
-            values = [
-                {
-                    "t": "2019-03-26T16:11:28.033+01:00",
-                    "v": 60
-                },
-                {
-                    "t": "2019-03-26T16:11:29.034+01:00",
-                    "v": 4
-                }]
-        }
-        else {
-            values = jsonData;
-        }
-        timeArray = values.map(element => element.t)
-        valuesArray = values.map(element => element.v)
-        timeArray.unshift('x')
-        valuesArray.unshift('Sensor0185')
-        console.log("jsonData: " + jsonData)
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                type: "area",
-                x: 'x',
-                xFormat: '%Y-%m-%dT%H:%M:%S.%L%Z', // 'xFormat' can be used as custom format of 'x'
-                columns: [
-                    timeArray,
-                    valuesArray
-                ]
-            },
-            axis: {
-                x: {
-                    type: 'timeseries',
-                    tick: {
-                        format: '%Y-%m-%d %H:%M:%S.%L%Z'
-                    }
-                }
-            }
-        });
-        return chart;
-    }
 
     function getDevices() {
         $.ajax({
@@ -125,8 +59,9 @@
             success: function (result) {
                 console.info(result);
                 appVue2.jsonData = result;
-                //console.info("appVue2.jsonData: " + appVue2.jsonData);
             }
         });
     }
+
+
 });
--- IOTConnect.Solution/IOTConnect.WebAPI/wwwroot/js/devices.js
+++ IOTConnect.Solution/IOTConnect.WebAPI/wwwroot/js/devices.js
@@ -1,9 +1,13 @@
 ﻿$(function () {
 
+    getAppVue2();
+    var chart = getAppChart();
+    chart.columns
     initActions();
-
     console.info("devices.js done");
 
+    //field
+    var itemsArray = [];
     // -- functions
 
     function initActions() {
@@ -13,8 +17,16 @@
             .configureLogging(signalR.LogLevel.Trace)
             .build();
 
+
         connection.on("MqttReceived", function (topic, message) {
-            console.log(topic, message);
+            //console.log(topic, message);
+            var text = getSelectedText('selection');
+            if (topic == text) {
+                itemsArray.push(JSON.parse(message))
+                removeDups(itemsArray);
+                updateChart(chart, topic, itemsArray)
+            }
+
         });
 
         connection.start();
@@ -22,5 +34,98 @@
         //    .catch(function (err) {
         //    return console.error(err.toString());
         //});
+    }
+
+    function getAppVue2() {
+        return new Vue({
+            el: '#app-vue2',
+            data: {
+                jsonData: null
+            }
+        });
+    }
+
+
+
+    function getSelectedText(elementId) {
+        var elt = document.getElementById(elementId);
+
+        if (elt.selectedIndex == -1)
+            return null;
+
+        return elt.options[elt.selectedIndex].text;
+    }
+
+    function updateChart(chartObject, name, jsonData) {
+
+        var timeArray = [];
+        var valuesArray = [];
+
+
+        timeArray = jsonData.map(element => element.timestamp)
+        valuesArray = jsonData.map(element => element.value)
+
+        timeArray.unshift('x')
+        valuesArray.unshift(name)
+
+        chartObject.load({
+            columns: [timeArray, valuesArray]
+        });
+        //console.info("!!!valuesArray length!!!!!" + valuesArray.length);
+        //var chart = c3.generate({
+        //    bindto: '#chart',
+        //    data: {
+        //        type: "area",
+        //        x: 'x',
+        //        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ', // 'xFormat' can be used as custom format of 'x'
+        //        columns: [
+        //            timeArray,
+        //            valuesArray
+        //        ]
+        //    },
+        //    axis: {
+        //        x: {
+        //            type: 'timeseries',
+        //            tick: {
+        //                format: '%Y-%m-%d %H:%M:%S.%L'
+        //            }
+        //        }
+        //    }
+        //});
+
+    }
+    function getAppChart() {
+
+        var timeArray = [];
+        var valuesArray = [];
+
+        var chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                type: "area",
+                x: 'x',
+                xFormat: '%Y-%m-%dT%H:%M:%S.%LZ', // 'xFormat' can be used as custom format of 'x'
+                columns: []
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d %H:%M:%S.%L'
+                    }
+                }
+            }
+        });
+        return chart;
+    }
+
+    function removeDups(items) {
+        let unique = {};
+        items.forEach(function (i) {
+            if (!unique[i]) {
+                unique[i] = true;
+            }
+        });
+        return Object.keys(unique);
     }
 });
